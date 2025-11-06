1357314e58f84b61c4c51551258edd597c6aeb25
bluebasecasinoBot <developerme33@gmail.com>
2025-11-06 14:43:40 +0100

    Update paths.js

--- paths.js
+++ paths.js
@@ -1,3 +1,4 @@
 import { SYD_VAR, SYD_VAR_constructor } from "./sydneyDom_v3.js";
 
-SYD_VAR.dev = new SYD_VAR_constructor({value:"http://localhost:3000/api"})
+
+SYD_VAR.dev = new SYD_VAR_constructor({value:"https://clothingsserver.fly.dev/api"})
